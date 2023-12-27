99c6bbb03f4256eaecc7e64e5c51755b07027396
Gerardo Lanza <glanza007@gmail.com>
2023-12-27 10:37:00 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoEcommerce/Models/EcommerceContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoEcommerce.Models.Entidades;
+
+namespace ProyectoEcommerce.Models
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext(DbContextOptions<EcommerceContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Categoria>().HasIndex(c => c.Nombre).IsUnique();
+        }
+    }
+}
